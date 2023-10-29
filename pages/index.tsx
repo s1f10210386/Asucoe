0f908d1d657274ee661ad6a1997d62279eb95a86
s1f10210386 <s1f102103864@iniad.org>
2023-10-30 01:57:47 +0900

    localstrage完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { useAtom } from "jotai";
 import { commentBoxShowAtom, showModelAtom } from "@/utils/jotai";
 import { useEffect, useState } from "react";
 import { ShowModel } from "@/component/Home/ShowModel/ShowModel";
+
 export default function Home(){
     const [showModel, setShowModel] = useAtom(showModelAtom);
     const [commentBoxShow, setCommentBoxShow] = useAtom(commentBoxShowAtom);
