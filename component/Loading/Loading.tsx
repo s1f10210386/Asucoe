b9da00a33e380f1a60e187738662be8a36625bd5
s1f10210386 <s1f102103864@iniad.org>
2023-11-03 04:10:53 +0900

    icon

--- component/Loading/Loading.tsx
+++ component/Loading/Loading.tsx
@@ -4,10 +4,15 @@ import Image from "next/image"
 
 export const Loading =()=>{
     return(
+        
         <div className={styles.container}>
-            {/* <div className={styles.icon}/> */}
+            {/* <div className={styles.mozi}>
+                Asucoe
+            <link rel="stylesheet" href="https://use.typekit.net/olh3xss.css"></link>
+            </div> */}
+            
             <div className={styles.icon}>
-                <Image src="/icon/asucoe_icon1.png" width={240} height={240} alt="asucoe" />
+                <Image src="/icon/asucoe_icon1.png" width={155} height={155} alt="asucoe" />
             </div>
             {/* <div className={styles.load}>
                 <CircularProgress/>
