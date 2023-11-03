19762e17f637c560f83534216a39f2201b5e3130
s1f10210254 <s1f10210248@iniad.org>
2023-11-03 19:44:51 +0900

    count追加

--- component/Home/ShowModel/ShowModel.tsx
+++ component/Home/ShowModel/ShowModel.tsx
@@ -1,4 +1,4 @@
-import { showModelAtom } from '@/utils/jotai';
+import { countAtom, showModelAtom } from '@/utils/jotai';
 import { motion } from 'framer-motion';
 import { useAtom } from 'jotai';
 import styles from './ShowModel.module.css'
@@ -6,23 +6,21 @@ import styles from './ShowModel.module.css'
 export const ShowModel = () => {
     const [showModel] = useAtom(showModelAtom)
 
+    const [count, setCount] = useAtom(countAtom);
+
     //質問内容
     //今日はハッカソン当日で、いまちょうど発表の途中でてが震えて緊張しています。
 
-    const answer = `
-    大丈夫ですよ！ハッカソンの発表は緊張するものですが、
-    あなたがこれまでにやってきた努力と成果を伝える大切な機会です。
-    深呼吸をして、自分を信じてください。皆さんもあなたのプロジェクトに興味を持って聞いています。
-    頑張ってください！発表が終わった後の達成感も楽しみにしてくださいね。
-    `
+    
+    const answer = `ハッカソン頑張れ！`
     return (
         <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: showModel ? 1 : 0 }}
             transition={{ duration: 2 }}
             className={styles.container}>
-            {/* {answer} */}
-            今日も１日お疲れさまでした。
+            {answer}
+            {/* 今日も１日お疲れさまでした。 */}
         </motion.div>
     )
 }
