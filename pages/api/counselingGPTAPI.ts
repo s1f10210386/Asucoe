df578de8742ceac6b74a303601e3c6b5a28cf9e5
s1f10210386 <s1f102103864@iniad.org>
2023-11-05 21:14:12 +0900

    a

--- pages/api/counselingGPTAPI.ts
+++ pages/api/counselingGPTAPI.ts
@@ -1,14 +1,15 @@
 import { counselingGPT } from "@/utils/counselingGPT";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function  counselingGPTAPI(req:NextApiRequest, res:NextApiResponse) {
-    if(req.method === "POST"){
-        const { user,content} = req.body;
-        const counselingContent = await counselingGPT(user, content);
-        res.status(200).json(counselingContent);
-    }else{
-        res.status(405).end()
-    }
+export default async function counselingGPTAPI(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { user, content } = req.body;
+    const counselingContent = await counselingGPT(user, content);
+    res.status(200).json(counselingContent);
+  } else {
+    res.status(405).end();
+  }
 }
-
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,137 +1,140 @@
-import { CommentBox } from "@/component/Home/CommentBox/CommentBox"
-import styles from "./index.module.css"
-import { Main } from "@/component/Home/Main/Main"
-import { TopBar } from "@/component/Home/TopBar/TopBar"
+import { CommentBox } from "@/component/Home/CommentBox/CommentBox";
+import styles from "./index.module.css";
+import { Main } from "@/component/Home/Main/Main";
+import { TopBar } from "@/component/Home/TopBar/TopBar";
 import { useAtom } from "jotai";
-import { TimeDataAtom, UserAtom, commentBoxShowAtom, hasLoadedOnceAtom, showModelAtom } from "@/utils/jotai";
+import {
+  TimeDataAtom,
+  UserAtom,
+  commentBoxShowAtom,
+  hasLoadedOnceAtom,
+  showModelAtom,
+} from "@/utils/jotai";
 import { useEffect, useState } from "react";
 import { ShowModel } from "@/component/Home/ShowModel/ShowModel";
 import { Loading } from "@/component/Loading/Loading";
 import Link from "next/link";
 import { baseURL } from "@/utils/url";
 
-export default function Home(){
+export default function Home() {
   const [hasLoadedOnce, setHasLoadedOnce] = useAtom(hasLoadedOnceAtom);
-  const [isLoading, setIsLoading] = useState(!hasLoadedOnce)
-  useEffect(()=>{
-    if(!hasLoadedOnce){
-      const timer = setTimeout(()=>{
-          setIsLoading(false);
-          setHasLoadedOnce(true)
+  const [isLoading, setIsLoading] = useState(!hasLoadedOnce);
+  useEffect(() => {
+    if (!hasLoadedOnce) {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+        setHasLoadedOnce(true);
       }, 2000);
 
-      return ()=>{
-          clearTimeout(timer);
-      }
+      return () => {
+        clearTimeout(timer);
+      };
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[hasLoadedOnce])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasLoadedOnce]);
 
-
-    const [showModel, setShowModel] = useAtom(showModelAtom);
-    const [commentBoxShow, setCommentBoxShow] = useAtom(commentBoxShowAtom);
-    useEffect(() => {
-      if (showModel) {
-          const timer = setTimeout(() => {
-              setShowModel(false);
-          }, 4000);
-          return () => {
-              clearTimeout(timer);
-          };
-      }
-    }, [showModel, setShowModel]);
-
-    const getDate = () => {
-      const date = new Date()
-      const currentHour = date.getHours();
-      // console.log("currentHour",currentHour)
-      const currentMinutes = date.getMinutes()
-  
-      const result = currentHour * 100 + currentMinutes
-      return result;
+  const [showModel, setShowModel] = useAtom(showModelAtom);
+  const [commentBoxShow, setCommentBoxShow] = useAtom(commentBoxShowAtom);
+  useEffect(() => {
+    if (showModel) {
+      const timer = setTimeout(() => {
+        setShowModel(false);
+      }, 4000);
+      return () => {
+        clearTimeout(timer);
+      };
     }
+  }, [showModel, setShowModel]);
 
-    //現在の時刻を記録
-    const [currentDateTime, setCurrentDateTime] = useState(getDate());
+  const getDate = () => {
+    const date = new Date();
+    const currentHour = date.getHours();
+    // console.log("currentHour",currentHour)
+    const currentMinutes = date.getMinutes();
 
-    //設定時刻
-    const [TimeData] = useAtom(TimeDataAtom)
-    useEffect(()=>{
-      // setCurrentDateTime(getCurrentTimestamp());
-    
-      const intervalId = setInterval(() => {
-        const date = getDate();
-        setCurrentDateTime(date);
-        if (currentDateTime >= TimeData){
-          setCommentBoxShow(true)
-        }
-      }, 60 * 1000);
-    
-      return ()=>{
-        clearInterval(intervalId)
+    const result = currentHour * 100 + currentMinutes;
+    return result;
+  };
+
+  //現在の時刻を記録
+  const [currentDateTime, setCurrentDateTime] = useState(getDate());
+
+  //設定時刻
+  const [TimeData] = useAtom(TimeDataAtom);
+  useEffect(() => {
+    // setCurrentDateTime(getCurrentTimestamp());
+
+    const intervalId = setInterval(() => {
+      const date = getDate();
+      setCurrentDateTime(date);
+      if (currentDateTime >= TimeData) {
+        setCommentBoxShow(true);
       }
-    },[setCommentBoxShow,setCurrentDateTime,currentDateTime, TimeData])
+    }, 60 * 1000);
 
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [setCommentBoxShow, setCurrentDateTime, currentDateTime, TimeData]);
 
+  //Userの基本情報をDBから取得
+  const [user, setUser] = useAtom(UserAtom);
+  const getUser = async () => {
+    const response = await fetch(`${baseURL}/api/getDB`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    const DB_user = data.user;
+    return DB_user;
+  };
 
-    //Userの基本情報をDBから取得
-    const [user, setUser] = useAtom(UserAtom)
-    const getUser = async()=>{
-      const response = await fetch(`${baseURL}/api/getDB`,{
-          method: "GET",
-          headers:{
-              'Content-Type': 'application/json',
-          },
-      })
-      const data = await response.json()
-      const DB_user = data.user;
-      return DB_user
-    }
+  useEffect(() => {
+    const fetchUser = async () => {
+      const DB_user = await getUser();
+      const userToSet = {
+        name: DB_user[0].name,
+        gender: DB_user[0].gender,
+        age: DB_user[0].age,
+        profession: DB_user[0].profession,
+      };
 
-    useEffect(()=>{
-      const fetchUser = async()=>{
-          const DB_user = await getUser()
-          const userToSet = {
-              name: DB_user[0].name,
-              gender: DB_user[0].gender,
-              age: DB_user[0].age,
-              profession: DB_user[0].profession,
-          }
-
-          setUser(userToSet);
-        
-      }
-      fetchUser();
-      
-    },[setUser])
+      setUser(userToSet);
+    };
+    fetchUser();
+  }, [setUser]);
 
   return (
     <div className={styles.container}>
-      {isLoading ?(
-          <div><Loading/></div>
-      ):(
+      {isLoading ? (
+        <div>
+          <Loading />
+        </div>
+      ) : (
         <>
           {showModel && (
-            <div> <ShowModel /></div>
+            <div>
+              {" "}
+              <ShowModel />
+            </div>
           )}
           {!showModel && (
             <div>
               <TopBar />
               <Main />
-              {/* <CommentBox />   */}
-              {commentBoxShow && (
+              <CommentBox />
+              {/* {commentBoxShow && (
                 <div>
                   <CommentBox />
                 </div>        
               )}
-            
+             */}
             </div>
           )}
         </>
-
       )}
-      
-         
     </div>
-  )
+  );
 }
